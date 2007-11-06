d4d60e7116d901c5c6cb80bbb6096c1152172c81
skabet <skabet@e8d86255-353e-0410-b69d-8de709201ba9>
2007-11-06 17:22:03 +0000

    Arbejd Arbejd

---
+++ tdd-alphamon/src/hotgammon/domain/AlphamonGame.java
@@ -0,0 +1,120 @@
+package hotgammon.domain;
+
+import java.util.Iterator;
+
+public class AlphamonGame implements Game {
+
+    /** Reset the entire game to start from scratch.  No player is in
+     * turn, and the game awaits a call to nextTurn to start a game.
+     * @see nextTurn
+     */
+    public void newGame(){
+    }
+
+    /** Tell the game instance that the user wants the game to progress
+     * after the 'turn has finished' i.e. the player in turn has
+     * exhausted his/her abilities for moving checkers.  The game
+     * responds by changing to the other player and rolling the dice. At
+     * the beginning of the game (after 'newGame') the dice are rolled
+     * and depending on the roll, the game progresses. NOTE that if the
+     * dice rolled are equal in value (like 1,1) then the game state is
+     * STILL in the initialisation phase (no player is in turn); thus
+     * you must repeatedly invoke nextTurn() until the dice rolled are
+     * not equal. The first non-equal valued dice rolled results in a
+     * player being defined as the player to move first. If die 1 > die
+     * 2 then red starts; otherwise it is black that moves first.
+     * 
+     * PRECONDITION: The player in turn has indeed exhausted the
+     * his/her ability to make moves.
+     */
+    public void nextTurn(){
+    }
+
+    /** Move one checker from one location to another.
+     * @param from the location to move the stone from
+     * @param to the location to move the stone to
+     * @return false if the indicated move is illegal 
+     */
+    public boolean move(Location from, Location to){
+        return false;
+    }
+
+    /** Return the color of the player that is in turn i.e. is allowed
+     * to make a move. 
+     * @return the color of the player to move next or Color.NONE if the
+     * turn is exhausted (that is, getNumberOfMovesLeft == 0).
+     */
+    public Color getPlayerInTurn(){
+        return null;
+    }
+
+    /** Return the number of moves left in the current turn. I.e. if a
+     * new turn has just begun and the dice rolled are [1,2] the return
+     * value is 2. If dice rolled are [3,3] then the return value is 4
+     * (if using standard backgammon rules). The number of moves left
+     * are subject to the backgammon rules, that is, if the player that
+     * has just rolled cannot use the dice values at all (for instance
+     * in situations where a checker is in the bar and the points are
+     * blocked for the given rolled dice) then the return value is 0.
+     * @return the number of moves left for the player-in-turn subject
+     * to the rules of backgammon.
+     */
+    public int getNumberOfMovesLeft(){
+        return 0;
+    }
+
+    /** Return an integer array of size exactly 2 containing the
+     * values of the dice thrown.
+     * @return array of two integers defining the dice values rolled
+     * last.
+     */
+    public int[] diceThrown(){
+        return null;
+    }
+
+    /** Return an integer array of size in range [0;4] containing die
+     * values that have not yet been used to move a checker.  Example:
+     * If returned value is [3,3,3] then there are three moves left for
+     * the player in turn, all of distance 3. If returned value is [2]
+     * then only one move is left of distance 2.
+     *
+     * POSTCONDITION: The array is sorted so the largest die value is
+     * first in the array.
+     * @return int array of unused die values.
+     */
+    public int[] diceValuesLeft(){
+        return null;
+    }
+
+    /** Return the winner of this game.
+     * @return the winner of the game; if game is still in progress, then
+     * Color.NONE is returned.
+     */
+    public Color winner(){
+        return null;
+    }
+
+    /** Return an iterator that allows inspecting the board.
+     * @return an iterator over the locations in the board.
+     */
+    public Iterator<Location> boardIterator(){
+        return null;
+    }
+
+    /** Get the colour of the checkers on a given location.
+     * @param location the location on the board to check
+     * @return the color of the checkers on this location
+     */
+    public Color getColor(Location location){
+        return null;
+    }
+
+    /** Get the count of checkers of this location.
+     * @param location the location to inspect
+     * @return a integer value showing the number of checkers on this location.
+     */
+    public int getCount(Location location){
+        return 0;
+    }
+}
+
--- tdd-alphamon/test/hotgammon/domain/TestBoard.java
+++ tdd-alphamon/test/hotgammon/domain/TestBoard.java
@@ -6,26 +6,94 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 /** Testcases for Backgammon Board.
-    Author: (c) Henrik Brbak Christensen 2007
+Author: (c) Henrik Brbak Christensen 2007
 */
 
 public class TestBoard {
 
-  private Board board;
-  
-  @Before public void setUp() { 
-  }
+    private Board board;
 
-  @Test
-  public void testIterator() {
-    Board b = new BoardImpl();
-    Iterator<Location> it = b.iterator();
-    assertNotNull(it);
-  }
+    @Before public void setUp() { 
+        board = new StandardBoard();
+    }
+    
+    /** Test of iterator(). Checking for:
+     *  - Not null
+     *  - Size = 28
+     *  - Location not null
+     *  - Location order (correct index)
+     */
+    @Test public void testIterator() {
+        Iterator<Location> it = board.iterator();
+        assertNotNull(it);
+        int i = 0;
+        while(it.hasNext())
+        {
+            Location l = it.next();
+            assertNotNull(l);
+            assertEquals(l.getIndex(),i);
+            i++;
+        }
+        assertEquals(i,28);
+    }
 
-  /** This wrapper is only required for running the old JUnit 3.8
-   * graphical user interface on new JUnit 4 test cases */
-  public static junit.framework.Test suite() {
-    return new junit.framework.JUnit4TestAdapter(TestBoard.class);
-  } 
+    /** Test of checkers. Checking for:
+     *  - 30 checkers
+     *  - Max 5 checkers at each spike-Location
+     */
+    @Test public void testCheckers() {
+        Iterator<Location> it = board.iterator();
+        int i = 0;
+        while(it.hasNext())
+        {
+            Location l = it.next();
+            i+= board.getCount(l);
+            if(l != Location.B_BAR && l != Location.R_BAR 
+                    && l != Location.B_BEAR_OFF && l != Location.R_BEAR_OFF)
+                assertTrue(board.getCount(l) < 6);
+
+        }
+        assertEquals(i,30);
+    }
+
+    /** Test of getColor(). Checking for:
+     *  - Not null
+     *  - Default colors
+     */
+    @Test public void testColors() {
+        Iterator<Location> it = board.iterator();
+        while(it.hasNext())
+        {
+            Location l = it.next();
+            assertNotNull(board.getColor(l));
+        }
+        assertEquals(board.getColor(Location.B1),Color.RED);
+        assertEquals(board.getColor(Location.R1),Color.BLACK);
+        assertEquals(board.getColor(Location.B2),Color.NONE);
+
+    }
+
+    @Test public void testMove() {
+        board.move(Location.R6,Location.R5);
+        assertEquals(board.getCount(Location.R5),1);
+        assertEquals(board.getCount(Location.R6),4);
+        assertEquals(board.getColor(Location.R5),board.getColor(Location.R6));
+        board.move(Location.R5,Location.R6);
+        assertEquals(board.getCount(Location.R6),5);
+        assertEquals(board.getCount(Location.R5),0);
+        assertEquals(board.getColor(Location.R5),Color.NONE);
+    }
+
+    @Test public void testReset() {
+        board.move(Location.R6,Location.R5);
+        board.reset();
+        assertEquals(board.getCount(Location.R5),0);
+        assertEquals(board.getColor(Location.R5),Color.NONE);
+    }
+
+    /** This wrapper is only required for running the old JUnit 3.8
+     * graphical user interface on new JUnit 4 test cases */
+    public static junit.framework.Test suite() {
+        return new junit.framework.JUnit4TestAdapter(TestBoard.class);
+    } 
 }
