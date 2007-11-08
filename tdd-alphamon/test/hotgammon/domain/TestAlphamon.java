393fd10c8e946d9271f6c48f5297e4a05ea08959
danamlund <danamlund@e8d86255-353e-0410-b69d-8de709201ba9>
2007-11-08 14:04:52 +0000

    added 5 more testcases for alphamon, implemented dice-stuff, fixed newGame and newGame tests, should be complete now as far as i can see

--- tdd-alphamon/test/hotgammon/domain/TestAlphamon.java
+++ tdd-alphamon/test/hotgammon/domain/TestAlphamon.java
@@ -16,161 +16,206 @@ import org.junit.Test;
 
 public class TestAlphamon {
 
-	private Game game;
+    private Game game;
 
-	@Before
-	public void setup() {
-		game = new AlphamonGame();
-		game.newGame();
+    @Before public void setup() {
+	game = new AlphamonGame();
+	game.newGame();
+	game.nextTurn();
+    }
+
+    /**
+     * the dice rolls are not random. The first two dice rolls give [1,2], the
+     * next two [3,4], next [5,6], and then it starts all over again.
+     */
+    @Test public void testDiceThrown() {
+	testDie(game.diceThrown(), 1, 2);
+	game.nextTurn();
+	testDie(game.diceThrown(), 1, 2);
+	game.nextTurn();
+	testDie(game.diceThrown(), 3, 4);
+	game.nextTurn();
+	testDie(game.diceThrown(), 3, 4);
+	game.nextTurn();
+	testDie(game.diceThrown(), 5, 6);
+	game.nextTurn();
+	testDie(game.diceThrown(), 1, 2);
+	game.nextTurn();
+    }
+
+    /**
+     * newGame = playerInturn == Color.Black
+     */
+    @Test
+        public void testNewGame() {
+	game.newGame();
+	assertEquals(Color.NONE, game.getPlayerInTurn());
+    }
+
+    /**
+     * nextTurn() -> ny spiller
+     */
+    @Test
+        public void testNextPlayer() {
+
+	assertEquals(Color.BLACK, game.getPlayerInTurn());
+	game.nextTurn();
+	assertEquals(Color.RED, game.getPlayerInTurn());
+	game.nextTurn();
+	assertEquals(Color.BLACK, game.getPlayerInTurn());
+    }
+
+    /**
+     * Test of iterator(). Checking for: - Not null - Size = 28 - Location not
+     * null - Location order (correct index)
+     */
+    @Test
+        public void testIterator() {
+	Iterator<Location> it = game.boardIterator();
+	assertNotNull(it);
+	int i = 0;
+	while (it.hasNext()) {
+	    Location l = it.next();
+	    assertNotNull(l);
+	    assertEquals(l.getIndex(), i);
+	    i++;
 	}
+	assertEquals(i, 28);
+    }
 
-	/**
-	 * the dice rolls are not random. The first two dice rolls give [1,2], the
-	 * next two [3,4], next [5,6], and then it starts all over again.
-	 */
-	@Test
-	public void testDiceThrown() {
+    /**
+     * Test of checkers. Checking for: - 30 checkers - Max 5 checkers at each
+     * spike-Location
+     */
+    @Test
+        public void testCheckers() {
+	Iterator<Location> it = game.boardIterator();
+	int i = 0;
+	while (it.hasNext()) {
+	    Location l = it.next();
+	    i += game.getCount(l);
+	    if (l != Location.B_BAR && l != Location.R_BAR
+		&& l != Location.B_BEAR_OFF && l != Location.R_BEAR_OFF)
+		assertTrue(game.getCount(l) < 6);
 
-		int[] d = game.diceThrown();
-		testDie(d, 1, 2);
-
-		d = game.diceThrown();
-		testDie(d, 3, 4);
-
-		d = game.diceThrown();
-		testDie(d, 3, 4);
-
-		d = game.diceThrown();
-		testDie(d, 5, 6);
-
-		d = game.diceThrown();
-		testDie(d, 1, 2);
-
-		d = game.diceThrown();
-		testDie(d, 3, 4);
-
-		d = game.diceThrown();
-		testDie(d, 3, 4);
-
-		d = game.diceThrown();
-		testDie(d, 5, 6);
 	}
+	assertEquals(i, 30);
+    }
 
-	/**
-	 * newGame = playerInturn == Color.Black
-	 */
-	@Test
-	public void testNewGame() {
-		assertEquals(Color.BLACK, game.getPlayerInTurn());
+    /**
+     * Test of getColor(). Checking for: - Not null - Default colors
+     */
+    @Test
+        public void testColors() {
+	Iterator<Location> it = game.boardIterator();
+	while (it.hasNext()) {
+	    Location l = it.next();
+	    assertNotNull(game.getColor(l));
 	}
+	assertEquals(game.getColor(Location.B1), Color.RED);
+	assertEquals(game.getColor(Location.R1), Color.BLACK);
 
-	/**
-	 * nextTurn() -> ny spiller
-	 */
-	@Test
-	public void testNextPlayer() {
+	assertEquals(game.getColor(Location.B2), Color.NONE);
+    }
 
-		game.nextTurn();
-		assertEquals(Color.RED, game.getPlayerInTurn());
+    private void testDie(int[] d, int a, int b) {
+	assertEquals(a, d[0]);
+	assertEquals(b, d[1]);
+    }
+    private String atos(int[] arr) {
+	String out = "";
+	for(int i=0; i<arr.length; i++) {
+	    out += arr[i]+"_";
 	}
+	return out;
+    }
 
-	/**
-	 * Test of iterator(). Checking for: - Not null - Size = 28 - Location not
-	 * null - Location order (correct index)
-	 */
-	@Test
-	public void testIterator() {
-		Iterator<Location> it = game.boardIterator();
-		assertNotNull(it);
-		int i = 0;
-		while (it.hasNext()) {
-			Location l = it.next();
-			assertNotNull(l);
-			assertEquals(l.getIndex(), i);
-			i++;
-		}
-		assertEquals(i, 28);
-	}
+    @Test
+        public void numberOfMovesLeftBeforeMove() {
+	assertEquals(2, game.getNumberOfMovesLeft());
+    }
 
-	/**
-	 * Test of checkers. Checking for: - 30 checkers - Max 5 checkers at each
-	 * spike-Location
-	 */
-	@Test
-	public void testCheckers() {
-		Iterator<Location> it = game.boardIterator();
-		int i = 0;
-		while (it.hasNext()) {
-			Location l = it.next();
-			i += game.getCount(l);
-			if (l != Location.B_BAR && l != Location.R_BAR
-					&& l != Location.B_BEAR_OFF && l != Location.R_BEAR_OFF)
-				assertTrue(game.getCount(l) < 6);
+    /**
+     * black vil always start in alphamon
+     */
+    @Test
+        public void onlyStartPlayerMoves() {
+	assertTrue(game.move(Location.R1, Location.R3));
+	assertEquals(1, game.getCount(Location.R3));
+	assertEquals(1, game.getCount(Location.R1));
+    }
 
-		}
-		assertEquals(i, 30);
-	}
+    @Test
+        public void otherPlayerCantMove() {
+	assertFalse(game.move(Location.R6, Location.R3));
+	assertEquals(2, game.getCount(Location.R1));
+    }
 
-	/**
-	 * Test of getColor(). Checking for: - Not null - Default colors
-	 */
-	@Test
-	public void testColors() {
-		Iterator<Location> it = game.boardIterator();
-		while (it.hasNext()) {
-			Location l = it.next();
-			assertNotNull(game.getColor(l));
-		}
-		assertEquals(game.getColor(Location.B1), Color.RED);
-		assertEquals(game.getColor(Location.R1), Color.BLACK);
+    @Test
+        public void moreThanOneCheckerOnToLocation() {
+	assertFalse(game.move(Location.R1, Location.R6));
+    }
 
-		assertEquals(game.getColor(Location.B2), Color.NONE);
-	}
+    @Test
+	public void diceValuesLeft() {
+	assertEquals( this.atos(new int[] {2,1,0,0}), this.atos(game.diceValuesLeft()) );
+	assertTrue( game.move(Location.R1, Location.R3) );
+	assertEquals( this.atos(new int[] {1,0,0,0}), this.atos(game.diceValuesLeft()) );
+	assertTrue( game.move(Location.R1, Location.R2) );
+	assertEquals( this.atos(new int[] {0,0,0,0}), this.atos(game.diceValuesLeft()) );
 
-	private void testDie(int[] d, int a, int b) {
-		assertEquals(d[0], a);
-		assertEquals(d[1], b);
-	}
+	game.nextTurn();
 
-	@Test
-	public void numberOfMovesLeftBeforeMove() {
-		assertEquals(2, game.getNumberOfMovesLeft());
-	}
+	assertEquals( this.atos(new int[] {2,1,0,0}), this.atos(game.diceValuesLeft()) );
+	assertTrue( game.move(Location.B1, Location.B2) );
+	assertEquals( this.atos(new int[] {2,0,0,0}), this.atos(game.diceValuesLeft()) );
+	assertTrue( game.move(Location.B1, Location.B3) );
+	assertEquals( this.atos(new int[] {0,0,0,0}), this.atos(game.diceValuesLeft()) );
+    }
 
-	@Test
-	public void numberOfMovesLeftAfterMove() {
-		game.move(Location.R1, Location.R3);
-		assertEquals(1, game.getNumberOfMovesLeft());
-	}
+    @Test
+        public void numberOfMovesLeftAfterMove() {
+	game.move(Location.R1, Location.R3);
+	assertEquals(1, game.getNumberOfMovesLeft());
+	game.move(Location.R1, Location.R2);
+	assertEquals(0, game.getNumberOfMovesLeft());
+    }
 
-	/**
-	 * black vil always start in alphamon
-	 */
-	@Test
-	public void onlyStartPlayerMoves() {
-		assertTrue(game.move(Location.R1, Location.R3));
-		assertEquals(1, game.getCount(Location.R3));
-		assertEquals(1, game.getCount(Location.R1));
-	}
 
-	@Test
-	public void otherPlayerCantMove() {
-		assertFalse(game.move(Location.R6, Location.R3));
-		assertEquals(2, game.getCount(Location.R1));
-	}
+    @Test
+	public void getPlayerInTurnWithNoMoves() {
+	assertTrue( game.move(Location.R1, Location.R3) );
+	assertTrue( game.move(Location.R1, Location.R2) );
+	assertEquals( Color.NONE, game.getPlayerInTurn() );
+    }
 
-	@Test
-	public void moreThanOneCheckerOnToLocation() {
-		assertFalse(game.move(Location.R1, Location.R6));
-	}
+    @Test
+	public void winner() {
+	assertEquals( Color.NONE, game.winner() ); 
+	game.nextTurn();
+	assertEquals( Color.NONE, game.winner() ); 
+	game.nextTurn();
+	assertEquals( Color.NONE, game.winner() ); 
+	game.nextTurn();
+	assertEquals( Color.NONE, game.winner() ); 
+	game.nextTurn();
+	assertEquals( Color.NONE, game.winner() ); 
+	game.nextTurn();
+	assertEquals( Color.RED, game.winner() ); 
+    }
 
-	
+    @Test
+	public void moveWhenNoMoreMoves() {
+	assertTrue( game.move(Location.R1, Location.R3) );
+	assertTrue( game.move(Location.R1, Location.R2) );
+	assertFalse( game.move(Location.R3, Location.R4) );
+    }
 
-	/**
-	 * This wrapper is only required for running the old JUnit 3.8 graphical
-	 * user interface on new JUnit 4 test cases
-	 */
-	public static junit.framework.Test suite() {
-		return new junit.framework.JUnit4TestAdapter(TestAlphamon.class);
-	}
+    /**
+     * This wrapper is only required for running the old JUnit 3.8 graphical
+     * user interface on new JUnit 4 test cases
+     */
+    public static junit.framework.Test suite() {
+	return new junit.framework.JUnit4TestAdapter(TestAlphamon.class);
+    }
 }
