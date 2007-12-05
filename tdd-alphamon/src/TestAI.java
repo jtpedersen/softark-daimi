b4652724fbf0f656ccda470b1bbeef8727b7ff7d
j_toft@hotmail.com <j_toft@hotmail.com@e8d86255-353e-0410-b69d-8de709201ba9>
2007-12-05 22:27:21 +0000

    AI test

---
+++ tdd-alphamon/src/TestAI.java
@@ -0,0 +1,96 @@
+import hotgammon.domain.Color;
+import hotgammon.domain.Game;
+import hotgammon.domain.GameListener;
+import hotgammon.domain.Location;
+import hotgammon.domain.MonFactory;
+import hotgammon.domain.MoveStrategy;
+import hotgammon.domain.RealBackgammonFactory;
+import hotgammon.domain.RealBackgammonMoveStrategy;
+import hotgammon.view.BackgammonUserInterface;
+import hotgammon.view.MonUserInterface;
+import hotgammon.view.tools.MonTool;
+
+import java.util.Iterator;
+
+import minidraw.framework.DrawingEditor;
+
+public class TestAI implements GameListener {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        String title = null;
+        MonFactory factory = null;
+
+        factory = new RealBackgammonFactory(true);
+        title = "Real Backgammon";
+
+        BackgammonUserInterface ui = new MonUserInterface(title, factory);
+        DrawingEditor editor = ui.getEditor();
+
+        editor.open();
+        ui.getGame().addGameListener(new TestAI(ui, Color.BLACK));
+        ui.getGame().newGame();
+
+        editor.setTool(new MonTool(ui));
+    }
+
+    private BackgammonUserInterface ui;
+    private Color player;
+    private MoveStrategy ms;
+
+    public TestAI(BackgammonUserInterface ui, Color c) {
+        this.ui = ui;
+        this.player = c;
+        this.ms = new RealBackgammonMoveStrategy();
+    }
+
+    private void generateMove() {
+        Game game = ui.getGame();
+        Iterator<Location> itfrom = game.boardIterator();
+        while (itfrom.hasNext()) {
+            Location from = (Location) itfrom.next();
+            if (game.getColor(from) == player && game.getCount(from) > 0) {
+                Iterator<Location> itTo = game.boardIterator();
+                while (itTo.hasNext()) {
+                    Location to = (Location) itTo.next();
+                    if (this.isValidMove(from, to)) {
+                        int move = ms.isValidMove(game, from, to);
+                        if (move > 0) {
+                            game.move(from, to);
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public void boardChange() {
+        if (ui.getGame().winner() == Color.NONE)
+            return;
+        if (player == ui.getGame().getPlayerInTurn())
+            generateMove();
+
+    }
+
+    public void diceRolled() {
+
+        // checkState();
+
+    }
+
+    /**
+     * basic general movement thats not allowed as a *mon move
+     */
+    private boolean isValidMove(Location from, Location to) {
+        Game game = ui.getGame();
+        if (to == from || game.getColor(from) == Color.NONE || to == Location.R_BAR
+                || to == Location.B_BAR || game.getPlayerInTurn() != game.getColor(from))
+            return false;
+
+        return true;
+    }
+}
