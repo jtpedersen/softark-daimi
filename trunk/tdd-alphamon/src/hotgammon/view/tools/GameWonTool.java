349cb91e77736e343dcabaf39bb857b0de2d0120
danamlund <danamlund@e8d86255-353e-0410-b69d-8de709201ba9>
2007-12-05 22:17:16 +0000

    now says how many moves left in the GUI, and can now start a new game after a winner is found

--- trunk/tdd-alphamon/src/hotgammon/view/tools/GameWonTool.java
+++ trunk/tdd-alphamon/src/hotgammon/view/tools/GameWonTool.java
@@ -4,6 +4,8 @@ import hotgammon.domain.Color;
 import hotgammon.view.BackgammonUserInterface;
 import minidraw.standard.NullTool;
 
+import java.awt.event.MouseEvent;
+
 /**
  * Tool to specifically move checkers. Note that this tool is tightly coupled to
  * the BoardLayout as it uses its definitions to match graphical location with
@@ -18,9 +20,18 @@ public class GameWonTool extends NullTool {
      * 
      * @param ui
      */
+    
+    BackgammonUserInterface ui;
+
     public GameWonTool(BackgammonUserInterface ui) {
+        this.ui = ui;
         Color winner = ui.getGame().winner();
-        ui.getEditor().showStatus(winner + " won the game, congratulations");
+        ui.getEditor().showStatus(winner + " won the game, congratulations " +
+                                  "(click to start a new game)");
+    }
+
+    public void mouseDown(MouseEvent e, int x, int y) {
+        ui.getGame().newGame();
     }
 
 }
